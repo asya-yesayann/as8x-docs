302ab612ea71f23be2340a93801e430512a5c62b
Asya-Yesayan <109540533+Asya-Yesayan@users.noreply.github.com>
2024-06-25 11:18:34 +0400

    Update definition_code.cs

--- src/server_api/examples/ds/array_based/definition_code.cs
+++ src/server_api/examples/ds/array_based/definition_code.cs
@@ -3,25 +3,27 @@ using System.Threading;
 using System.Threading.Tasks;
 using ArmSoft.AS8X.Common.Extensions;
 using ArmSoft.AS8X.Common.FieldTypes;
-using ArmSoft.AS8X.Core.Constants;
+using ArmSoft.AS8X.Core;
+using ArmSoft.AS8X.Core.Document;
 using ArmSoft.AS8X.Core.DS;
-using ArmSoft.AS8X.Core.Properties;
 
-namespace ArmSoft.AS8X.Core.DSImplementation
+namespace ArmSoft.AS8X.Bank.General.DS
 {
-    [DataSource(nameof(ProcMode))]
-    public class ProcMode : DataSource<ProcMode.DataRow, ProcMode.Param>
+    [DataSource("DocFlds")]
+    public class DocumentFields : DataSource<DocumentFields.DataRow, DocumentFields.Param>
     {
         public class DataRow : IExtendableRow
         {
-            public short CODE { get; set; }
-            public string DESC { get; set; }
+            public string Name { get; set; }
+            public string Caption { get; set; }
             public object Extend { get; set; }
         }
+
         public class Param
         {
-            public short SourceType { get; set; }
+            public string DocType { get; set; }
         }
+
         public override bool IsSQLBased
         {
             get
@@ -29,39 +31,32 @@ namespace ArmSoft.AS8X.Core.DSImplementation
                 return false;
             }
         }
-        public ProcMode(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            this.Schema = new Schema(this.Name, ConstantsArmenian.ProcessingMode.ToArmenianANSICached(), ConstantsEnglish.ProcessingMode, typeof(DataRow), typeof(Param));
 
-            this.Schema.AddColumn(nameof(DataRow.CODE), "0", ConstantsArmenian.Code.ToArmenianANSICached(), ConstantsEnglish.Code, FieldTypeProvider.GetNumericFieldType(3), true);
-            this.Schema.AddColumn(nameof(DataRow.DESC), "1", ConstantsArmenian.Descript.ToArmenianANSICached(), ConstantsEnglish.Descript, FieldTypeProvider.GetStringFieldType(DSConstantsLength.PrModeDesc), true
-                        , showType: FieldTypeProvider.GetStringFieldType(DSConstantsLength.PrModeDescShow));
-            // Parameters
-            this.Schema.AddParam(nameof(Param.SourceType), ConstantsArmenian.Type.ToArmenianANSICached(), FieldTypeProvider.GetNumericFieldType(2), eDescription: ConstantsEnglish.Type);
+        private readonly IDBService dBService;
+        public DocumentFields(IDBService dBService, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            this.dBService = dBService;
+            this.Schema = new Schema(this.Name, "Փաստաթղթի դաշտեր".ToArmenianANSI(), "Document's fields", typeof(DataRow), typeof(Param));
+
+            this.Schema.AddColumn(nameof(DataRow.Name), "", "Կոդ".ToArmenianANSI(), "Code", FieldTypeProvider.GetStringFieldType(25));
+            this.Schema.AddColumn(nameof(DataRow.Caption), "", "Անվանում".ToArmenianANSI(), "Name", FieldTypeProvider.GetStringFieldType(30));
+
+            this.Schema.AddParam(nameof(Param.DocType), "Փաստաթղթի տեսակ".ToArmenianANSI(), FieldTypeProvider.GetStringFieldType(8), eDescription: "Document's type");
         }
 
-        protected override Task FillData(DataSourceArgs<Param> args, CancellationToken stoppingToken)
+        protected override async Task FillData(DataSourceArgs<Param> args, CancellationToken stoppingToken)
         {
-            this.Rows.AddRange(
-            [
-                new DataRow
+            var documentDescription = await DocumentHelper.DocumentDescription(this.dBService.Connection, args.Parameters.DocType);
+            foreach (var field in documentDescription.Fields)
+            {
+                var row = new DataRow
                 {
-                    CODE = 0,
-                    DESC = Resources.WithoutService
-                },
-                new DataRow
-                {
-                    CODE = 1,
-                    DESC = args.Parameters.SourceType == 0 ? Resources.DSProcessingMode1 : Resources.DocTemplateProcessingMode1
-                },
-                new DataRow
-                {
-                    CODE = 2,
-                    DESC = Resources.RunOnServer
-                }
-            ]
-            );
-            return Task.CompletedTask;
+                    Name = field.Key,
+                    Caption = field.Value.Caption
+                };
+                this.Rows.Add(row);
+            }
         }
     }
 }
+
