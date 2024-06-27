7a5859059c7f7c738a86944c5c037e5df9ff6c10
Asya-Yesayan <109540533+Asya-Yesayan@users.noreply.github.com>
2024-06-27 12:28:25 +0400

    Update array_based_code.cs

--- src/server_api/examples/ds/array_based_code.cs
+++ src/server_api/examples/ds/array_based_code.cs
@@ -3,62 +3,61 @@ using System.Threading;
 using System.Threading.Tasks;
 using ArmSoft.AS8X.Common.Extensions;
 using ArmSoft.AS8X.Common.FieldTypes;
-using ArmSoft.AS8X.Core.Document;
 using ArmSoft.AS8X.Core.DS;
+using ArmSoft.AS8X.Core.Tree;
 
 namespace ArmSoft.AS8X.Core.DSImplementation
 {
-    [DataSource("DocFlds")]
-    public class DocumentFields : DataSource<DocumentFields.DataRow, DocumentFields.Param>
+    [DataSource(nameof(TreeNode))]
+    public class TreeNode : DataSource<TreeNode.DataRow, TreeNode.Param>
     {
+        private readonly IDBService dBService;
+        private readonly TreeElementService treeElementService;
+        
         public class DataRow : IExtendableRow
         {
             public string Code { get; set; }
-            public string Caption { get; set; }
+            public string Name { get; set; }
             public object Extend { get; set; }
         }
 
         public class Param
         {
-            public string DocType { get; set; }
+            public string TreeId { get; set; }
+            public string NodeType { get; set; }
         }
 
-        public override bool IsSQLBased
+        public TreeNode(IDBService dbService, IServiceProvider serviceProvider) : base(serviceProvider)
         {
-            get
-            {
-                return false;
-            }
+            this.dBService = dbService;
+            this.Schema = new Schema(this.Name, "Ծառի հանգույցներ".ToArmenianANSI(), "Tree nodes", typeof(DataRow), typeof(Param));
+
+            this.Schema.AddColumn(nameof(DataRow.Code), "Code", "Կոդ".ToArmenianANSI(), "Code", FieldTypeProvider.GetStringFieldType(20));
+            this.Schema.AddColumn(nameof(DataRow.Name), "Name", "Անվանում".ToArmenianANSI(), "Name", FieldTypeProvider.GetStringFieldType(50));
+            // Parameters
+            this.Schema.AddParam(nameof(Param.TreeId), "Ծառի իդենտիֆիկատոր".ToArmenianANSI(), FieldTypeProvider.GetStringFieldType(4), eDescription: "TreeId");
+            this.Schema.AddParam(nameof(Param.NodeType), "Ծառի հանգույցներ".ToArmenianANSI(), FieldTypeProvider.GetStringFieldType(1), eDescription: "Tree nodes");
         }
 
-        private readonly IDBService dBService;
-        public DocumentFields(IDBService dBService, IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            this.dBService = dBService;
-            this.Schema = new Schema(this.Name, "Փաստաթղթի դաշտեր".ToArmenianANSI(), "Document's fields", typeof(DataRow), typeof(Param));
-
-            this.Schema.AddColumn(nameof(DataRow.Code), "", "Կոդ".ToArmenianANSI(), "Code", FieldTypeProvider.GetStringFieldType(25));
-            this.Schema.AddColumn(nameof(DataRow.Caption), "", "Անվանում".ToArmenianANSI(), "Name", FieldTypeProvider.GetStringFieldType(30));
-
-            this.Schema.AddParam(nameof(Param.DocType), "Փաստաթղթի տեսակ".ToArmenianANSI(), FieldTypeProvider.GetStringFieldType(8), eDescription: "Document's type");
-        }
+        public override bool IsSQLBased => false;
 
         protected override async Task FillData(DataSourceArgs<Param> args, CancellationToken stoppingToken)
         {
-            if (!string.IsNullOrWhiteSpace(args.Parameters.DocType))
+            string treeId = args.Parameters.TreeId;
+            if (!string.IsNullOrWhiteSpace(treeId))
             {
-                var documentDescription = await DocumentHelper.DocumentDescription(this.dBService.Connection, args.Parameters.DocType);
-                foreach (var field in documentDescription.Fields)
+                var treeNodes = await this.treeElementService.GetTreeElements(treeId, args.Parameters.NodeType);
+                foreach (var (code, treeNode) in treeNodes)
                 {
                     var row = new DataRow
                     {
-                        Code = field.Key,
-                        Caption = field.Value.Caption
+                        Code = treeNode.Key,
+                        Name = treeNode.Comment
                     };
                     this.Rows.Add(row);
                 }
             }
         }
+
     }
 }
-
